e9ea8be8785d42da63d5e6ddf0434ecb6b776f21
codexedoc <liarikhalid@gmail.com>
2019-10-03 14:38:40 +0000

    update person.cs

--- NinjectTest/Person.cs
+++ NinjectTest/Person.cs
@@ -7,6 +7,7 @@
         public int Age { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
 
         public Person(int age, string firstName, string lastName, IPersonFormatter personFormatter)
         {
